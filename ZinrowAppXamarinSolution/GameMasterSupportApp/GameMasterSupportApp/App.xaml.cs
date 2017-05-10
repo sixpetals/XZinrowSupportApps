9b19f2c96bf03f371ba4b06496f0ac0f24cf7324
SUGIMOTO Takuma <takuma.sugimoto@aegif.jp>
2017-05-10 16:41:07 +0900

    MVVM仕様のための修正

--- ZinrowAppXamarinSolution/GameMasterSupportApp/GameMasterSupportApp/App.xaml.cs
+++ ZinrowAppXamarinSolution/GameMasterSupportApp/GameMasterSupportApp/App.xaml.cs
@@ -2,27 +2,20 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-
+using Prism.Unity;
 using Xamarin.Forms;
+using GameMasterSupportApp.Views;
 
 namespace GameMasterSupportApp {
-    public partial class App : Application {
-        public App() {
+    public partial class App : PrismApplication {
+        public App(IPlatformInitializer initializer = null) : base(initializer) { }
+        protected override void OnInitialized() {
             InitializeComponent();
-
-            MainPage = new GameMasterSupportApp.Views.MainPage();
+            NavigationService.NavigateAsync("MainPage?title=Hello%20from%20Xamarin.Forms");
+        }
+        protected override void RegisterTypes() {
+            Container.RegisterTypeForNavigation<MainPage>();
         }
 
-        protected override void OnStart() {
-            // Handle when your app starts
-        }
-
-        protected override void OnSleep() {
-            // Handle when your app sleeps
-        }
-
-        protected override void OnResume() {
-            // Handle when your app resumes
-        }
     }
 }
