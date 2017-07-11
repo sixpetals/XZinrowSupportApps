79aaaa4659a15667238c30ab9afdff3673aa294a
SUGIMOTO Takuma <takuma.sugimoto@aegif.jp>
2017-07-11 21:58:27 +0900

    バックグラウンドを実装

--- ZinrowMaster/ZinrowMaster/Views/MainPage.xaml.cs
+++ ZinrowMaster/ZinrowMaster/Views/MainPage.xaml.cs
@@ -3,7 +3,11 @@
 namespace ZinrowMaster.Views {
     public partial class MainPage : ContentPage {
         public MainPage() {
+
+          
             InitializeComponent();
+            this.BackgroundImage.Source  = ImageSource.FromResource("ZinrowMaster.Resources.background.png");
+
         }
     }
 }
