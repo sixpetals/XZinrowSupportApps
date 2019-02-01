c775da3d277ae386b5781ee2c2b3006622ed57cd
SUGIMOTO Takuma <microstory@gmail.com>
2019-02-01 19:29:08 +0900

    スタイルの変更

--- ZinrowMaster/ZinrowMaster/Views/SecondPage.xaml.cs
+++ ZinrowMaster/ZinrowMaster/Views/SecondPage.xaml.cs
@@ -4,6 +4,9 @@ namespace ZinrowMaster.Views {
     public partial class SecondPage : ContentPage {
         public SecondPage() {
             InitializeComponent();
+
+            this.BgImage.Source = ImageSource.FromResource("ZinrowMaster.Resources.background.png");
+
         }
     }
 }
